01d127949e958c2e51885ab6bb685446697b0096
Thomas Keate <36827591+tomlkeate@users.noreply.github.com>
2023-07-29 17:05:28 +0000

    [LeetCode Sync] Runtime - 2 ms (3.06%), Memory - 6.6 MB (100.00%)

---
+++ 1603-running-sum-of-1d-array/solution.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    *returnSize = numsSize;
+    int runningSum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        runningSum += nums[i];
+        nums[i] = runningSum;
+    }
+    return nums;
+}
