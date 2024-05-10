c35f03bb83e992bfad8ec73067595b13465786f9
Thomas Keate <36827591+tomlkeate@users.noreply.github.com>
2024-05-10 07:20:05 +0000

    [LeetCode Sync] Runtime - 37 ms (0.11%), Memory - 16.6 MB (100.00%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        prev_one, prev_two = (1, 1)
-        for i in range(n - 1, 0, -1):
-            temp = prev_one
-            prev_one = prev_one + prev_two
-            prev_two = temp
-        return prev_one
+        if (n == 1):
+            return 1
+        one_prev = 1
+        two_prev = 1
+        total = 0
+        for i in range(n - 1):
+            total = one_prev + two_prev
+            two_prev = one_prev
+            one_prev = total
+        return total
