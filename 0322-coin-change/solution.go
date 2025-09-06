176667d4e3c41c7ba0a4a06dc3ecf721fa9890b3
Thomas Keate <36827591+tomlkeate@users.noreply.github.com>
2025-09-06 06:36:50 +0000

    [LeetCode Sync] Runtime - 10 ms (80.24%), Memory - 8.6 MB (56.84%)

---
+++ 0322-coin-change/solution.go
@@ -0,0 +1,19 @@
+func coinChange(coins []int, amount int) int {
+    dp := make([]int, amount + 1)
+    for i := range dp {
+        dp[i] = amount + 1
+    }
+    dp[0] = 0
+    for i := 1; i <= amount; i++ {
+        for _, coin := range coins {
+            if coin > i {
+                continue
+            }
+            dp[i] = min(dp[i], 1 + dp[i - coin])
+        }
+    }
+    if dp[amount] == amount + 1 {
+        return -1
+    }
+    return dp[amount]
+}
